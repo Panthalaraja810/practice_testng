2253a8ec6c54ae19c994d2fe53c6f3130ad87c45
raja <abc@gmail.com>
2023-04-25 21:59:28 +0530

    commit1

---
+++ src/test/java/MainEx.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class MainEx 
+{
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("test1 created");
+	}
+
+}
---
+++ src/test/java/MainEx2.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+
+public class MainEx2 
+{
+	@Test
+	public void test2()
+	{
+		System.out.println("test2 created");
+	}
+
+}
